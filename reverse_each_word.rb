56ac6413473e744ae657b1c7f4bdd7c9dd0cd711
yego <yegokipkemoi254@gmail.com>
2022-10-23 00:37:32 +0300

    Done

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,25 @@
 # Write your code here
+require 'pry'
+
+# def reverse_each_word(sentence)
+#     words = sentence.split
+#     reversed_words = []
+
+#     words.each do |word|
+#         reversed_words <<  word.reverse
+#      end
+#      reversed_words.join(" ")
+# end
+
+# puts reverse_each_word("Hello there, and how are you?")
+
+def reverse_each_word sentence
+    words = sentence.split
+
+    reversed_words = words.map {|word| word.reverse}
+
+    reversed_words.join(" ")
+
+end
+
+puts reverse_each_word("Hello there, and how are you?")
